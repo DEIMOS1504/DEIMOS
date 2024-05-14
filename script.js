3ea2374a308e85a777f43a117385c235fe6d3697
DEIMOS1504 <davidrealg4lite@gmail.com>
2024-05-14 13:39:34 -0500

    Add files via upload
    DETALLE 🥰

---
+++ script.js
@@ -0,0 +1,19 @@
+setTimeout(cambiaMensaje,2500)
+setTimeout(cambiaMensajo,5000)
+setTimeout(cambiaMensajes,8000)
+
+function cambiaMensaje() {
+    document.getElementById("loading-message").innerText = "ESPERA AMOR, YA CASI ❤️";
+}
+
+function cambiaMensajo() {
+    document.getElementById("loading-message").innerText = "ESPERA AMOR, YA FALTA POCO🥰";
+}
+
+function cambiaMensajes() {
+    document.getElementById("loading-message").innerText = "AHORA SI LEELO, TE AMOOOOOOO❤️❤️";
+}
+
+setTimeout(function () {
+    alert("Hola mi Reina❤️, Han pasado alrededor de 4 semanas y sinceramente he sentido un amor genuino,  unos días maravillosos, puedo afirmar que me has devuelto el sentimiento tan bonito de querer mucho a alguien, aprender amar sus cambios de humor y sobre todo resalto lo auténtica que eres...❤️, Haré muchas cosas por y para ti, para que sientas el amor que te tengo y seas muy feliz sintiéndote cómoda a mi lado. PINTAS DE COLORES MI VIDA, aunq muchas veces tenga que lidiar con mi caos, eres la chica que me ordena, una estrellita entre tantas nubes oscuras(como io jaja). Tal vez este siendo muy cursi, pero para hablar sobre lo que siento, tengo que ser sincero y ahora, a través de este texto, estoy manifestando todo lo que mi corazón siente y mi cabecita piensa ❤️, Magníficamente te amo.. , Gracias por todo lo que me has enseñado a ser. Deseo que sigamos pasando mas días maravillosos Te amo, muks 😚❤️")
+}, 10000)
